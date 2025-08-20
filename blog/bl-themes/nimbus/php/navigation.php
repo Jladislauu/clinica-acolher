333560092a653d77716442014f49f2f334d33287
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-08-20 13:52:31 -0300

    subindo blog corrigindo o head

--- blog/bl-themes/nimbus/php/navigation.php
+++ blog/bl-themes/nimbus/php/navigation.php
@@ -9,4 +9,5 @@
   </ul>
   <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
 </nav>
-<a class="btn-getstarted" href="<?php echo $site->url(); ?>">Home</a>
+<a class="btn-getstarted" href="<?php echo $site->url(); ?>">Início</a>
+<a class="btn-getstarted" href="https://lightgrey-manatee-733013.hostingersite.com/">Site</a>
